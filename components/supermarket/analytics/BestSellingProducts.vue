630c5a5e224d4aa441bc6eaf5f86c744a3553631
Montazer Malik <mntthr1122@gmail.com>
2021-08-16 01:09:27 +0300

    something

---
+++ components/supermarket/analytics/BestSellingProducts.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="py-1 mb-2 r-2 position-relative product">
+        <span class="position-absolute badge badge-light border-white text-dark count">
+            count34
+        </span>
+        <span class="position-absolute badge badge-light border-white text-dark left">
+            المتبقي : 787
+        </span> 
+        <div class="card-body pb-2 text-center">
+            <div class="row">
+                <span class="h5 text-light mb-1">
+                    name
+                </span>
+            </div>
+            <div class="row">
+                <span class="text-light text-left">
+                    4000 د.ع                                       
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .count{
+        top: -5px; left: -5px;
+    }
+    .left{
+        top: -5px; right: -5px;
+    }
+    .product{
+        border: 1px solid #fff4;
+    }
+    .product:hover{
+        cursor:pointer;
+        background: $t-1;
+    }
+    .card{
+        margin-bottom: 5px !important;
+    }
+</style>
--- pages/supermarket/analytics/index.vue
+++ pages/supermarket/analytics/index.vue
@@ -1,8 +1,16 @@
 <template>
     <div>
         <div class="container-fluid">
-            <div class="row main-height">
-                
+            <div class="row main-height bg-section">
+                <div class="col-lg-4">
+                    <SupermarketAnalyticsBestSellingProducts />
+                </div>
+                <div class="col-lg-4">
+                    <SupermarketAnalyticsBestSellingProducts />
+                </div>
+                <div class="col-lg-4">
+                    <SupermarketAnalyticsBestSellingProducts />
+                </div>
             </div>
         </div>
     </div>
@@ -18,5 +26,20 @@
 </script>
 
 <style lang="scss" scoped>
-// 
+    .count{
+        top: -5px; left: -5px;
+    }
+    .left{
+        top: -5px; right: -5px;
+    }
+    .product{
+        border: 1px solid #fff4;
+    }
+    .product:hover{
+        cursor:pointer;
+        background: $t-1;
+    }
+    .card{
+        margin-bottom: 5px !important;
+    }
 </style>
